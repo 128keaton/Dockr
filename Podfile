75099f35e97a924e0700f501bb9ccf15c4ec83c5
Keaton Burleson <keaton.burleson@me.com>
2015-02-22 17:07:45 -0600

    2.1.2 Pods fixed :(

---
+++ Podfile
@@ -0,0 +1,2 @@
+pod 'CRGradientNavigationBar'
+pod 'BEMSimpleLineGraph'
---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// BEMSimpleLineGraph
+#define COCOAPODS_POD_AVAILABLE_BEMSimpleLineGraph
+#define COCOAPODS_VERSION_MAJOR_BEMSimpleLineGraph 3
+#define COCOAPODS_VERSION_MINOR_BEMSimpleLineGraph 3
+#define COCOAPODS_VERSION_PATCH_BEMSimpleLineGraph 0
+
+// CRGradientNavigationBar
+#define COCOAPODS_POD_AVAILABLE_CRGradientNavigationBar
+#define COCOAPODS_VERSION_MAJOR_CRGradientNavigationBar 0
+#define COCOAPODS_VERSION_MINOR_CRGradientNavigationBar 3
+#define COCOAPODS_VERSION_PATCH_CRGradientNavigationBar 2
+
